a0d473041e625598b9477a77f7d77bd33bc50dc9
Rémi Gardien <gardien.remi@gmail.com>
2025-09-17 12:44:11 +0200

    chore(prettier): add prettier-plugin-tailwindcss (ESM config)

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss']
+}
+export default config
